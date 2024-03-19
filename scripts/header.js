0b959b52420846d49a5be3f311bfa69a85acc223
Nick-Guh <126421361+Nick-Guh@users.noreply.github.com>
2024-03-19 14:37:57 +0100

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,23 +1,25 @@
-hamburgerMenuButton = document.querySelector('.hamburger-menu');
+hamburgerMenuButton = document.querySelector('.open-sidebar-btn');
 
 sidebar = document.querySelector('.sidebar');
 spacer = document.querySelector('.spacer');
-main = document.querySelector('main');
 logoScuola = document.querySelector('.logo-scuola');
+logoTuttoFila = document.querySelector('.logo-tutto-fila');
 screenDarkener =document.querySelector('.screen-darkener');
 
 sidebarRelated = [
   sidebar,
   spacer,
-  main,
   logoScuola,
+  logoTuttoFila,
   screenDarkener
 ]
 
+mobileSearchInput = document.querySelector('.search-bar .mobile-search-input')
+searchBar = document.querySelector('.search-bar')
 
 
 hamburgerMenuButton.addEventListener('click', () => {showSidebar()});
-screenDarkener.addEventListener('click', () => {showSidebar()});
+
 
 function showSidebar(){
   if(sidebar.classList.contains('sidebar-visible')) {
@@ -30,3 +32,32 @@ function showSidebar(){
     });
   }
 }
+
+
+screenDarkener.addEventListener('click', () => {screenDarkenerPress()});
+
+function screenDarkenerPress(){
+  if(searchBar.classList.contains('cover')){
+    searchBar.classList.remove('cover')
+    screenDarkener.classList.remove('cover')
+  }
+  if(sidebar.classList.contains('sidebar-visible')){
+    sidebarRelated.forEach(element => {
+      element.classList.remove('sidebar-visible')
+    });
+  }
+}
+
+
+mobileSearchInput.addEventListener('click', () => {searchBarCover()});
+
+function searchBarCover(){
+  if(searchBar.classList.contains('cover')){
+    searchBar.classList.remove('cover')
+    screenDarkener.classList.remove('cover')
+  } else {
+    searchBar.classList.add('cover')
+    screenDarkener.classList.add('cover')
+  }
+}
+
